7c6b124f49403d1f5a0cddd261f4af42ad92874c
Crowdbotics <team@crowdbotics.com>
2021-01-20 08:53:53 +0000

    remove package blueprint-react-native-articles-screen

--- src/config/installed_blueprints.js
+++ src/config/installed_blueprints.js
@@ -1,7 +1,6 @@
 export const installed_blueprints = [
   //@BlueprintInsertion
 { name: 'BlankScreen19194739', human_name: 'Blank Screen 19', access_route: 'BlankScreen19194739'},
-{ name: 'ArticleList194722', human_name: 'Article List', access_route: 'ArticleList194722'},
 { name: 'ArticleList194721', human_name: 'Article List', access_route: 'ArticleList194721'},
 { name: 'ArticleList194720', human_name: 'Article List', access_route: 'ArticleList194720'},
 
--- src/navigator/mainNavigator.js
+++ src/navigator/mainNavigator.js
@@ -6,7 +6,6 @@ import SplashScreen from "../features/SplashScreen";
 import SideMenu from './sideMenu';
 //@BlueprintImportInsertion
 import BlankScreen19194739Navigator from '../features/BlankScreen19194739/navigator';
-import ArticleList194722Navigator from '../features/ArticleList194722/navigator';
 import ArticleList194721Navigator from '../features/ArticleList194721/navigator';
 import ArticleList194720Navigator from '../features/ArticleList194720/navigator';
 
@@ -18,7 +17,6 @@ const AppNavigator = {
 
     //@BlueprintNavigationInsertion
 BlankScreen19194739: { screen: BlankScreen19194739Navigator },
-ArticleList194722: { screen: ArticleList194722Navigator },
 ArticleList194721: { screen: ArticleList194721Navigator },
 ArticleList194720: { screen: ArticleList194720Navigator },
 
